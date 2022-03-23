31a11c0576094195463a6c11e6a54f58707de664
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2022-03-23 15:11:31 +0000

    models

--- src/00-workshop-demo-files/db/seed.js
+++ src/00-workshop-demo-files/db/seed.js
@@ -12,7 +12,21 @@ function getRandomItem(arr = []) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+async function seedBooks() {
+  await Promise.all([db.User.deleteMany({}), db.Book.deleteMany({})]);
+
+  const users = await db.User.insertMany([...getSeedUsers()]);
+  const userIds = users.map((user) => user._id);
+  const booksWithAuthors = [...getSeedBooks()].map((book) => ({
+    ...book,
+    author: getRandomItem(userIds),
+  }));
+
+  return db.Book.insertMany(booksWithAuthors);
+}
+
 module.exports = {
   seedUsers: seedUsers,
   getRandomItem: getRandomItem,
+  seedBooks: seedBooks,
 };
